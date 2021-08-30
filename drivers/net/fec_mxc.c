e87495276b8f5bd88b5cc6ca8c70cdfbd11475bf
hongbo.wang <hongbo.wang@nxp.com>
2021-08-30 17:18:48 +0800

    i.MX8M: Baremetal: FEC_MXC ethernet support
    Signed-off-by: hongbo.wang <hongbo.wang@nxp.com>
    Signed-off-by: Jerry Huang <jerry.huang@nxp.com>

--- drivers/net/fec_mxc.c
+++ drivers/net/fec_mxc.c
@@ -588,6 +588,7 @@ static int fec_open(struct eth_device *edev)
 	return 0;
 }
 
+static bool sfec_flag = true;
 #ifdef CONFIG_DM_ETH
 static int fecmxc_init(struct udevice *dev)
 #else
@@ -630,7 +631,7 @@ static int fec_init(struct eth_device *dev, struct bd_info *bd)
 	writel(0x00000000, &fec->eth->gaddr2);
 
 	/* Do not access reserved register */
-	if (!is_mx6ul() && !is_mx6ull() && !is_imx8() && !is_imx8m() && !is_imx8ulp() &&
+	if (sfec_flag && !is_mx6ul() && !is_mx6ull() && !is_imx8() && !is_imx8m() && !is_imx8ulp() &&
 	    !is_imx93()) {
 		/* clear MIB RAM */
 		for (i = mib_ptr; i <= mib_ptr + 0xfc; i += 4)
@@ -639,6 +640,9 @@ static int fec_init(struct eth_device *dev, struct bd_info *bd)
 		/* FIFO receive start register */
 		writel(0x520, &fec->eth->r_fstart);
 	}
+#if defined(CONFIG_TARGET_IMX8MM_EVK) || defined(CONFIG_TARGET_IMX8MP_EVK)
+	sfec_flag = false;
+#endif
 
 	/* size and address of each buffer */
 	writel(FEC_MAX_PKT_SIZE, &fec->eth->emrbr);
