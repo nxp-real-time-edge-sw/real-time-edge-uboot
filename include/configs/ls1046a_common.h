38efe8aef88dafc33708c06c84daa3a6199293d0
Xiaoliang Yang <xiaoliang.yang_1@nxp.com>
2021-08-30 12:18:22 +0800

    ls1046ardb: add configs to support baremetal on ls1046ardb
    Signed-off-by: Xiaoliang Yang <xiaoliang.yang_1@nxp.com>
    Signed-off-by: Changming Huang <jerry.huang@nxp.com>

--- include/configs/ls1046a_common.h
+++ include/configs/ls1046a_common.h
@@ -56,6 +56,7 @@
 #define CONFIG_SYS_MALLOC_LEN		(CONFIG_ENV_SIZE + 1024 * 1024)
 
 /* Serial Port */
+#define CONFIG_CONS_INDEX      2
 #define CONFIG_SYS_NS16550_SERIAL
 #define CONFIG_SYS_NS16550_REG_SIZE	1
 #define CONFIG_SYS_NS16550_CLK          (get_serial_clock())
@@ -135,6 +136,7 @@
 /* I2C */
 #if !CONFIG_IS_ENABLED(DM_I2C)
 #define CONFIG_SYS_I2C
+#define CONFIG_SYS_I2C_MXC
 #define CONFIG_SYS_I2C_MXC_I2C1		/* enable I2C bus 1 */
 #define CONFIG_SYS_I2C_MXC_I2C2		/* enable I2C bus 2 */
 #define CONFIG_SYS_I2C_MXC_I2C3		/* enable I2C bus 3 */
@@ -151,6 +153,7 @@
 
 #ifdef CONFIG_PCI
 #define CONFIG_PCI_SCAN_SHOW
+#define CONFIG_CMD_PCI
 #endif
 
 /* SATA */
