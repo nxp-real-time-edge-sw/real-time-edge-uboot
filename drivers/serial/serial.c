7e84c1775151592c94d4d71feeccb3ae2fe60300
Xiaoliang Yang <xiaoliang.yang_1@nxp.com>
2021-08-30 12:12:37 +0800

    ls1043ardb: serial: support core id debug
    1. Support core id print debug on console.
    2. Add read write lock on serial puts driver.
    3. Change to use uart2 as serial output.
    
    Signed-off-by: Xiaoliang Yang <xiaoliang.yang_1@nxp.com>
    Signed-off-by: Changming Huang <jerry.huang@nxp.com>

--- drivers/serial/serial.c
+++ drivers/serial/serial.c
@@ -19,6 +19,7 @@ DECLARE_GLOBAL_DATA_PTR;
 
 static struct serial_device *serial_devices;
 static struct serial_device *serial_current;
+extern sgd_t *sgd;
 /*
  * Table with supported baudrates (defined in config_xyz.h)
  */
@@ -436,7 +437,13 @@ void serial_putc(const char c)
  */
 void serial_puts(const char *s)
 {
+#ifdef CONFIG_ENABLE_WRITE_LOCK
+	arch_write_lock(&sgd->consol_lock_puts);
+#endif
 	get_current()->puts(s);
+#ifdef CONFIG_ENABLE_WRITE_LOCK
+	arch_write_unlock(&sgd->consol_lock_puts);
+#endif
 }
 
 /**
