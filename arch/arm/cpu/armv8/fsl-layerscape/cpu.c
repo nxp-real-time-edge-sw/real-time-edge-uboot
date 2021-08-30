f8bd14c41e2acde57cd6ce0b888281a72840fa6b
xu chen <xu.chen_3@nxp.com>
2021-08-30 17:04:13 +0800

    lx2160ardb: add the baremetal support
    Signed-off-by: xu chen <xu.chen_3@nxp.com>
    Signed-off-by: Changming Huang <jerry.huang@nxp.com>

--- arch/arm/cpu/armv8/fsl-layerscape/cpu.c
+++ arch/arm/cpu/armv8/fsl-layerscape/cpu.c
@@ -1330,6 +1330,7 @@ phys_size_t get_effective_memsize(void)
 		ea_size = gd->ram_size;
 	}
 
+#ifndef CONFIG_ARCH_LX2160A
 #ifdef CONFIG_SYS_MEM_RESERVE_SECURE
 	/* Check if we have enough space for secure memory */
 	if (ea_size > CONFIG_SYS_MEM_RESERVE_SECURE)
@@ -1345,6 +1346,7 @@ phys_size_t get_effective_memsize(void)
 		else
 			printf("Error: No enough space for reserved memory.\n");
 	}
+#endif
 
 	return ea_size;
 }
