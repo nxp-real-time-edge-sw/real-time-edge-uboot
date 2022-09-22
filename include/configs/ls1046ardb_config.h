55fee8b8a8ae28ec7b330bc6b552d85fef6a1062
Minghuan Lian <Minghuan.Lian@nxp.com>
2022-09-22 14:15:28 +0800

    baremetal: Add baremtal defconfig
    Signed-off-by: Minghuan Lian <Minghuan.Lian@nxp.com>

--- include/configs/ls1046ardb_config.h
+++ include/configs/ls1046ardb_config.h
@@ -5,7 +5,7 @@
  * SPDX-License-Identifier:	GPL-2.0+
  */
 
-#define CONFIG_SLAVE_FIRST_CORE                 1
+#define CONFIG_ICC
 
 /* set Master core size to 512M, slave core size to 256M, share mem to 256M */
 #define CONFIG_SYS_DDR_SDRAM_SLAVE_SIZE	(256 * 1024 * 1024)
@@ -14,18 +14,12 @@
 #define CONFIG_SYS_DDR_SDRAM_SHARE_SIZE \
 	((256 * 1024 * 1024) - CONFIG_SYS_DDR_SDRAM_SHARE_RESERVE_SIZE)
 
-#define CONFIG_SYS_TEXT_BASE    0x84000000
+#define CONFIG_SYS_DDR_SDRAM_SHARE_BASE \
+	(CONFIG_SYS_DDR_SDRAM_BASE + CONFIG_SYS_DDR_SDRAM_MASTER_SIZE \
+	+ CONFIG_SYS_DDR_SDRAM_SLAVE_SIZE * (CONFIG_MAX_CPUS - 1))
 
-/*
- * Size of malloc() pool
- */
-#define CONFIG_SYS_MALLOC_LEN	(CONFIG_ENV_SIZE + 128 * 1024 * 1024)
+#define CONFIG_SYS_DDR_SDRAM_SHARE_RESERVE_BASE \
+	(CONFIG_SYS_DDR_SDRAM_SHARE_BASE + CONFIG_SYS_DDR_SDRAM_SHARE_SIZE)
 
 /* Number of cores in each cluster */
 #define CORE_NUM_PER_CLUSTER    4
-
-/* I2C */
-#define CONFIG_SYS_I2C_MXC_I2C1	/* enable I2C bus 0 */
-#define CONFIG_SYS_I2C_MXC_I2C2	/* enable I2C bus 1 */
-#define CONFIG_SYS_I2C_MXC_I2C3	/* enable I2C bus 2 */
-#define CONFIG_SYS_I2C_MXC_I2C4	/* enable I2C bus 3 */
