3ccfd4647f36d92e96021b61b1beca6cb5377e2e
Chunguang Li <chunguang.li_1@nxp.com>
2023-01-10 11:48:58 +0800

    baremetal: rtc: fsl-ftm-alarm: add FTM alarm driver
    For the platforms including LS1021A, LS1046A that has the
    Flextimer module, implementing alarm function to generate
    cyclicity interrupt.
    
    Signed-off-by: Chunguang Li <chunguang.li_1@nxp.com>
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

---
+++ include/ftm_alarm.h
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Freescale FlexTimer Module (FTM) alarm device driver.
+ *
+ * Copyright 2023 NXP
+ *
+ */
+
+#ifndef __FTM_ALARM_H
+#define __FTM_ALARM_H
+
+#include <stdbool.h>
+#include <common.h>
+#include <linux/types.h>
+#include <linux/bitops.h>
+#include <asm/interrupt-gic.h>
+
+typedef void (*ftm_irq_func)(void *);
+
+void ftm_counter_enable(struct udevice *dev);
+void ftm_counter_disable(struct udevice *dev);
+u64 ftm_get_count(struct udevice *dev);
+void ftm_reset_count(struct udevice *dev);
+
+void ftm_rtc_set_alarm(struct udevice *dev, u16 ticks, void (* func)(void *));
+void ftm_rtc_alarm_stop(struct udevice *dev);
+
+#endif
