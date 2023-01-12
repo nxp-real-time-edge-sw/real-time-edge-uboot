f800eeb5950f798d240816a6cf7436b8d6dba082
Tao Yang <Tao.Yang1@nxp.com>
2023-01-12 17:26:44 +0800

    ls1043ardb: add jailhouse bootcmd

--- include/configs/ls1043a_common.h
+++ include/configs/ls1043a_common.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0+ */
 /*
  * Copyright (C) 2015 Freescale Semiconductor
- * Copyright 2019-2021 NXP
+ * Copyright 2019-2021, 2023 NXP
  */
 
 #ifndef __LS1043A_COMMON_H
@@ -171,6 +171,11 @@
 #include <config_distro_bootcmd.h>
 #endif
 
+#define JAILHOUSE_ENV \
+	"jh_mmcboot=setenv dtb fsl-ls1043a-rdb-sdk-jailhouse.dtb;" \
+		"setenv othbootargs mem=1024MB;" \
+		"run bootcmd \0"
+
 /* Initial environment variables */
 #define CONFIG_EXTRA_ENV_SETTINGS		\
 	"hwconfig=fsl_ddr:bank_intlv=auto\0"	\
@@ -198,6 +203,7 @@
 	"boot_os=y\0"				\
 	"mtdparts=" CONFIG_MTDPARTS_DEFAULT "\0"	\
 	BOOTENV					\
+	JAILHOUSE_ENV \
 	"boot_scripts=ls1043ardb_boot.scr\0"	\
 	"boot_script_hdr=hdr_ls1043ardb_bs.out\0"	\
 	"scan_dev_for_boot_part="		\
