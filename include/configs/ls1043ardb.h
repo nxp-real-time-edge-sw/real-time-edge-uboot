8dc35b7ae2f94b291ec8978baadf683261162c2b
Xiaoliang Yang <xiaoliang.yang_1@nxp.com>
2023-01-12 17:26:46 +0800

    baremetal: ls1043ardb: Add BareMetal setting
    Signed-off-by: Xiaoliang Yang <xiaoliang.yang_1@nxp.com>
    Signed-off-by: Minghuan Lian <Minghuan.Lian@nxp.com>
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

--- include/configs/ls1043ardb.h
+++ include/configs/ls1043ardb.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0+ */
 /*
  * Copyright 2015 Freescale Semiconductor
- * Copyright 2022 NXP
+ * Copyright 2022-2023 NXP
  */
 
 #ifndef __LS1043ARDB_H__
@@ -210,5 +210,7 @@
 	"image=Image\0"					\
 	"console_dbg=earlycon=uart8250,mmio,0x21c0500\0"
 #endif
+
+#include "ls1043a_baremetal.h"
 
 #endif /* __LS1043ARDB_H__ */
