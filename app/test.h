971576a6dff2599844d06951e97f904d8c669ce1
Xiaoliang Yang <xiaoliang.yang_1@nxp.com>
2023-01-12 17:26:47 +0800

    baremetal: add test gpio app
    Add GPIO driver on layerscape platform.
    
    Signed-off-by: Xiaoliang Yang <xiaoliang.yang_1@nxp.com>
    Signed-off-by: Changming Huang <jerry.huang@nxp.com>

---
+++ app/test.h
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright 2018-2023 NXP
+ *
+ */
+
+#ifndef _BAREMETAL_APP_TEST_H
+#define _BAREMETAL_APP_TEST_H
+
+#ifndef __ASSEMBLY__
+
+int test_gpio(void);
+
+#endif
+#endif /* _BAREMETAL_APP_TEST_H */
