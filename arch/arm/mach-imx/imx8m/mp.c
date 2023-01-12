c915e40df28fb8ab8eebc8323f04095c2a8d29f5
Jiafei Pan <Jiafei.Pan@nxp.com>
2023-01-12 17:26:46 +0800

    mp: imx8m: add PSCI based cpu release command
    Current CPU release command is using spintable, but
    it is abandaoned, so change the default command to
    use PSCI API.
    
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

--- arch/arm/mach-imx/imx8m/mp.c
+++ arch/arm/mach-imx/imx8m/mp.c
@@ -9,7 +9,10 @@
 #include <common.h>
 #include <cpu_func.h>
 #include <asm/io.h>
+#include <asm/ptrace.h>
+#include <asm/system.h>
 #include <linux/errno.h>
+#include <linux/psci.h>
 #include <asm/arch/sys_proto.h>
 #include <asm/arch/imx-regs.h>
 
@@ -142,6 +145,8 @@ void imx_set_cpu_pwr_on(unsigned int core_id)
 	mmio_setbits_32(IMX_SRC_BASE + IMX8M_SRC_A53RCR1, (1 << core_id));
 }
 
+#ifdef CONFIG_BAREMETAL
+/* Currently baremetal is using spintable for cpu release */
 int cpu_release(u32 cpuid, int argc, char *const argv[])
 {
 	uint64_t boot_addr;
@@ -153,6 +158,30 @@ int cpu_release(u32 cpuid, int argc, char *const argv[])
 
 	return 0;
 }
+#else
+/* PSCI method to kick secondary cores */
+int cpu_release(u32 nr, int argc, char *const argv[])
+{
+	u64 boot_addr;
+
+	boot_addr = simple_strtoull(argv[0], NULL, 16);
+
+	/* Use PSCI to kick the core */
+	struct pt_regs regs;
+
+	printf("begin to kick cpu core #%d to address %llx\n",
+			nr, boot_addr);
+	regs.regs[0] = PSCI_0_2_FN64_CPU_ON;
+	regs.regs[1] = nr;
+	regs.regs[2] = boot_addr;
+	regs.regs[3] = 0;
+	smc_call(&regs);
+	if (regs.regs[0])
+		return -1;
+
+	return 0;
+}
+#endif
 
 int is_core_valid(unsigned int core)
 {
