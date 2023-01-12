91809bf24f82c0fbfbeaa333daaba4af3db48000
Minghuan Lian <Minghuan.Lian@nxp.com>
2023-01-12 17:26:47 +0800

    baremetal: imx8mmevk: Enable uart4 for slave cores
    Signed-off-by: Minghuan Lian <Minghuan.Lian@nxp.com>
    RTEv3.5:
    	board/freescale -> board/nxp
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

--- board/nxp/imx8mm_evk/imx8mm_evk.c
+++ board/nxp/imx8mm_evk/imx8mm_evk.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0+
 /*
- * Copyright 2018 NXP
+ * Copyright 2018, 2023 NXP
  */
 
 #include <config.h>
@@ -27,6 +27,11 @@
 static iomux_v3_cfg_t const uart_pads[] = {
 	IMX8MM_PAD_UART2_RXD_UART2_RX | MUX_PAD_CTRL(UART_PAD_CTRL),
 	IMX8MM_PAD_UART2_TXD_UART2_TX | MUX_PAD_CTRL(UART_PAD_CTRL),
+};
+
+static iomux_v3_cfg_t const uart4_pads[] = {
+       IMX8MM_PAD_UART4_RXD_UART4_RX | MUX_PAD_CTRL(UART_PAD_CTRL),
+       IMX8MM_PAD_UART4_TXD_UART4_TX | MUX_PAD_CTRL(UART_PAD_CTRL),
 };
 
 static iomux_v3_cfg_t const wdog_pads[] = {
@@ -96,8 +101,10 @@ int board_early_init_f(void)
 	set_wdog_reset(wdog);
 
 	imx_iomux_v3_setup_multiple_pads(uart_pads, ARRAY_SIZE(uart_pads));
+	imx_iomux_v3_setup_multiple_pads(uart4_pads, ARRAY_SIZE(uart4_pads));
 
 	init_uart_clk(1);
+	init_uart_clk(3);
 
 #ifdef CONFIG_NAND_MXS
 	setup_gpmi_nand(); /* SPL will call the board_early_init_f */
