9da67b416887f47408bcf24d463293c957c78724
Chunguang Li <chunguang.li_1@nxp.com>
2023-01-12 17:26:52 +0800

    Add SAI support for ls1028ardb BareMetal
    Signed-off-by: Chunguang Li <chunguang.li_1@nxp.com>
    RTEv3.5 conflicts:
    	arch/arm/config.mk
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

--- arch/arm/config.mk
+++ arch/arm/config.mk
@@ -26,7 +26,9 @@ PLATFORM_RELFLAGS += $(call cc-option, -msoft-float) \
       $(call cc-option,-mshort-load-bytes,$(call cc-option,-malignment-traps,))
 
 ifeq ($(CONFIG_ARM64),y)
+ifneq ($(CONFIG_SOUND_SGTL5000),y)
 PLATFORM_RELFLAGS += $(call cc-option,-mgeneral-regs-only)
+endif
 endif
 
 # LLVM support
