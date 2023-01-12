aebe8e04a4f677edfe3b40e31ac5427ad6f65ea4
Minghuan Lian <Minghuan.Lian@nxp.com>
2023-01-12 17:26:46 +0800

    baremetal: change master core ram size
    Signed-off-by: Minghuan Lian <Minghuan.Lian@nxp.com>

--- common/board_f.c
+++ common/board_f.c
@@ -7,6 +7,8 @@
  * (C) Copyright 2002
  * Sysgo Real-Time Solutions, GmbH <www.elinos.com>
  * Marius Groeger <mgroeger@sysgo.de>
+ *
+ * Copyright 2023 NXP
  */
 
 #include <common.h>
@@ -326,6 +328,10 @@ static int setup_dest_addr(void)
 	 * Ram is setup, size stored in gd !!
 	 */
 	debug("Ram size: %08lX\n", (ulong)gd->ram_size);
+#if defined(CONFIG_BAREMETAL)
+	if (get_core_id() == 0)
+		gd->ram_size = CONFIG_BAREMETAL_SYS_SDRAM_MASTER_SIZE;
+#endif
 #if defined(CONFIG_SYS_MEM_TOP_HIDE)
 	/*
 	 * Subtract specified amount of memory to hide so that it won't
