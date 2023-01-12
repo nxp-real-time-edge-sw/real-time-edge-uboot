a275188f45174c3c35df1118e7a047e664a545a6
Hang Zhang <hang.zhang_1@nxp.com>
2023-01-12 17:59:24 +0800

    baremetal: add gpio interrupt test driver
    Signed-off-by: Hang Zhang <hang.zhang_1@nxp.com>

---
+++ include/gpio_int.h
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Freescale FlexTimer Module (FTM) alarm device driver.
+ *
+ * Copyright 2023 NXP
+ *
+ */
+
+#ifndef __GPIO_INT_H
+#define __GPIO_INT_H
+
+void gpio_int_enable(void);
+
+void gpio_int_start(void);
+
+void gpio_int_stop(void);
+
+
+#endif
