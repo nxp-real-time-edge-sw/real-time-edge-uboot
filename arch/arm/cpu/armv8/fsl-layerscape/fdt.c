69fd62955d6058ba154327d3d989e355189cf381
Minghuan Lian <Minghuan.Lian@nxp.com>
2023-01-12 17:26:48 +0800

    baremetal: fman: fix the compile issue without DM
    Signed-off-by: Minghuan Lian <Minghuan.Lian@nxp.com>
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

--- arch/arm/cpu/armv8/fsl-layerscape/fdt.c
+++ arch/arm/cpu/armv8/fsl-layerscape/fdt.c
@@ -624,6 +624,7 @@ void fdt_fixup_pfe_firmware(void *blob)
 #endif
 
 #if defined(CONFIG_TARGET_LS1046ARDB) || defined(CONFIG_TARGET_LS1043ARDB)
+#ifdef CONFIG_DM_ETH
 #include <net.h>
 
 struct ec_port_t {
@@ -821,6 +822,7 @@ loop_tag:
 	}
 }
 #endif
+#endif
 
 void ft_cpu_setup(void *blob, struct bd_info *bd)
 {
@@ -901,7 +903,9 @@ void ft_cpu_setup(void *blob, struct bd_info *bd)
 #endif
 
 #if defined(CONFIG_TARGET_LS1046ARDB) || defined(CONFIG_TARGET_LS1043ARDB)
+#ifdef CONFIG_DM_ETH
 	fdt_fixup_ethercat_prop(blob);
+#endif
 #endif
 }
 
