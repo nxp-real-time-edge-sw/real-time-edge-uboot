4eb001ef41a0e7e90d75a29487a40d3b1d91c71f
Chunguang Li <chunguang.li_1@nxp.com>
2023-01-12 17:26:47 +0800

    baremetal: imx8mpevk: Add 15 sec delay before kernel booting
    Add 15 sec delay before kernel booting to avoid the issue
    which kernel and BareMetal will hang during network init
    when fec net port disconnect.
    
    Signed-off-by: Chunguang Li <chunguang.li_1@nxp.com>

--- include/configs/imx8mp_evk.h
+++ include/configs/imx8mp_evk.h
@@ -96,7 +96,7 @@
 	"bm_addr=50200000\0" \
 	"loadbmimage=fatload mmc ${mmcdev}:${mmcpart} ${bm_addr} ${bmimage}\0" \
 	"startbm=dcache flush;cpu 1 release 50200000;sleep 6; \
-		cpu 2 release 50200000; sleep 2; cpu 3 release 50200000;sleep 2\0" \
+		cpu 2 release 50200000; sleep 2; cpu 3 release 50200000;sleep 17\0" \
 	"boot_bm=" \
 		"if run loadbmimage; then " \
 			"run startbm; "	\
