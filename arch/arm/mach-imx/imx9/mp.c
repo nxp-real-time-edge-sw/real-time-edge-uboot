0011c3cf27cc3818a8976027c88b05f5edd9da8b
Jiafei Pan <Jiafei.Pan@nxp.com>
2024-09-23 17:04:41 +0800

    imx9: mp: add cpu disable and cpu status support
    Add cpu status command support to retrieve CPU Core status, add cpu
    disable command support to bring specified CPU Core to be off.
    
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>

--- arch/arm/mach-imx/imx9/mp.c
+++ arch/arm/mach-imx/imx9/mp.c
@@ -4,15 +4,14 @@
  */
 
 #include <common.h>
-#include <cpu_func.h>
 #include <asm/ptrace.h>
 #include <asm/system.h>
 #include <linux/psci.h>
-#include <asm/arch/sys_proto.h>
-#include <asm/arch/imx-regs.h>
 
 #define MPIDR_MT_BIT		BIT(24)
 #define MPIDR_AFF1_SHFT		(8)
+
+#define IMX_SIP_CPU_OFF		0xC200000D
 
 static u64 logical_id_to_hwid(unsigned int core)
 {
@@ -39,14 +38,67 @@ int cpu_reset(u32 nr)
 
 int cpu_disable(u32 nr)
 {
-	printf("Feature is not implemented.\n");
+	struct pt_regs regs;
+
+	if (nr >= CONFIG_MAX_CPUS) {
+		printf("Invalid CPU ID %d\n", nr);
+		return -1;
+	}
+
+	regs.regs[0] = PSCI_0_2_FN_AFFINITY_INFO;
+	regs.regs[1] = logical_id_to_hwid(nr);
+	regs.regs[2] = 0;
+	regs.regs[3] = 0;
+	smc_call(&regs);
+	if (regs.regs[0] == PSCI_0_2_AFFINITY_LEVEL_OFF) {
+		printf("CPU Core %d is already off\n", nr);
+		return 0;
+	}
+
+	regs.regs[0] = IMX_SIP_CPU_OFF;
+	regs.regs[1] = nr;
+	regs.regs[2] = 0;
+	regs.regs[3] = 0;
+	smc_call(&regs);
+	if (regs.regs[0]) {
+		printf("Failed to disable CPU Core %d, please check whether TF-A supports SIP CPU off service: %lx\n",
+				nr, regs.regs[0]);
+		return -1;
+	}
+	printf("CPU core #%d is disabled\n", nr);
+
         return 0;
 }
 
 int cpu_status(u32 nr)
 {
-	printf("Feature is not implemented.\n");
-	return 0;
+	struct pt_regs regs;
+
+	if (nr >= CONFIG_MAX_CPUS) {
+		printf("Invalid CPU ID %d\n", nr);
+		return -1;
+	}
+
+	regs.regs[0] = PSCI_0_2_FN_AFFINITY_INFO;
+	regs.regs[1] = logical_id_to_hwid(nr);
+	regs.regs[2] = 0;
+	regs.regs[3] = 0;
+	smc_call(&regs);
+	switch (regs.regs[0]) {
+	case PSCI_0_2_AFFINITY_LEVEL_ON:
+		printf("CPU Core %d is running\n", nr);
+		return 0;
+	case PSCI_0_2_AFFINITY_LEVEL_OFF:
+		printf("CPU Core %d is off\n", nr);
+		return 0;
+	case PSCI_0_2_AFFINITY_LEVEL_ON_PENDING:
+		printf("CPU Core %d is pending\n", nr);
+		return 0;
+	}
+
+	printf("CPU Core %d status is unknown\n", nr);
+
+	return -1;
 }
 
 int cpu_release(u32 nr, int argc, char *const argv[])
@@ -57,6 +109,16 @@ int cpu_release(u32 nr, int argc, char *const argv[])
 	if (nr >= CONFIG_MAX_CPUS) {
 		printf("Invalid CPU ID %d\n", nr);
 		return -1;
+	}
+
+	regs.regs[0] = PSCI_0_2_FN_AFFINITY_INFO;
+	regs.regs[1] = logical_id_to_hwid(nr);
+	regs.regs[2] = 0;
+	regs.regs[3] = 0;
+	smc_call(&regs);
+	if (regs.regs[0] == PSCI_0_2_AFFINITY_LEVEL_ON) {
+		printf("CPU Core %d is already running, disalbe it firstly before release it\n", nr);
+		return 0;
 	}
 
 	boot_addr = simple_strtoull(argv[0], NULL, 16);
