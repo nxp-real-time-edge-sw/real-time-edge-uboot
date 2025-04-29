4cf4090436d5524c6a0374bb548738e0f2cfc1ba
Hou Zhiqiang <Zhiqiang.Hou@nxp.com>
2025-04-30 01:29:38 +0800

    baremetal: common: console: fix build warning
    common/console.c:738:13: warning: ‘puts_to_console’ defined but not used [-Wunused-function]
     static void puts_to_console(const char *s)
    	     ^~~~~~~~~~~~~~~
    
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

--- common/console.c
+++ common/console.c
@@ -753,6 +753,7 @@ void putc(const char c)
 	}
 }
 
+#ifdef CONFIG_ENABLE_COREID_DEBUG
 static void puts_to_console(const char *s)
 {
 	if (gd->flags & GD_FLG_DEVINIT) {
@@ -764,6 +765,7 @@ static void puts_to_console(const char *s)
 		serial_puts(s);
 	}
 }
+#endif
 
 void puts(const char *s)
 {
