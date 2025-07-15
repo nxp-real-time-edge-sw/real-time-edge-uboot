0fff1f82ca2b3328263371fe685578119e777a9d
Anuj <anuj@axisbinc.com>
2025-07-15 07:04:18 +0000

    Modified gpio pin number. Added led_pads setup in imx8mp_evk.c

--- board/freescale/imx8mp_evk/imx8mp_evk.c
+++ board/freescale/imx8mp_evk/imx8mp_evk.c
@@ -33,6 +33,7 @@
 DECLARE_GLOBAL_DATA_PTR;
 #define UART_PAD_CTRL	(PAD_CTL_DSE6 | PAD_CTL_FSEL1)
 #define WDOG_PAD_CTRL	(PAD_CTL_DSE6 | PAD_CTL_ODE | PAD_CTL_PUE | PAD_CTL_PE)
+#define LED_PAD_CTRL  (PAD_CTL_DSE6 | PAD_CTL_FSEL1)
 
 static iomux_v3_cfg_t const uart_pads[] = {
 	MX8MP_PAD_UART2_RXD__UART2_DCE_RX | MUX_PAD_CTRL(UART_PAD_CTRL),
@@ -41,6 +42,10 @@ static iomux_v3_cfg_t const uart_pads[] = {
 
 static iomux_v3_cfg_t const wdog_pads[] = {
 	MX8MP_PAD_GPIO1_IO02__WDOG1_WDOG_B  | MUX_PAD_CTRL(WDOG_PAD_CTRL),
+};
+
+static iomux_v3_cfg_t const led_pads[] = {
+  MX8MP_PAD_NAND_READY_B__GPIO3_IO16 | MUX_PAD_CTRL(LED_PAD_CTRL),
 };
 
 #ifdef CONFIG_NAND_MXS
@@ -80,6 +85,7 @@ int board_early_init_f(void)
 
 	init_uart_clk(1);
 
+	imx_iomux_v3_setup_multiple_pads(led_pads, ARRAY_SIZE(led_pads));
 	return 0;
 }
 
