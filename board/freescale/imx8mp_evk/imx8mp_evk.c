c8a682aef40c823989ed407d223e80a4e4dab8df
Anuj <anuj@axisbinc.com>
2025-07-29 09:28:38 +0000

    Removed led toggle code from board init

--- board/freescale/imx8mp_evk/imx8mp_evk.c
+++ board/freescale/imx8mp_evk/imx8mp_evk.c
@@ -489,11 +489,6 @@ int board_init(void)
 	init_usb_clk();
 #endif
 
-#ifdef CONFIG_LED_STATUS
-	status_led_init();
-	status_led_set(CONFIG_LED_STATUS_BIT, CONFIG_LED_STATUS_ON);
-#endif
-
 	return 0;
 }
 
